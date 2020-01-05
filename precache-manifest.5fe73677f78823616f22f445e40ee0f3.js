35617d9f216aa3d8ffc5ca403f7e35e3b254788e
kunalbhatia18 <kunal18042002@gmail.com>
2020-01-05 17:39:52 +0530

    Updates

---
+++ precache-manifest.5fe73677f78823616f22f445e40ee0f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d5ce2a5f02da3bae564d24c35fdf35",
+    "url": "/clip-art/index.html"
+  },
+  {
+    "revision": "ee49bd88c40a3b27bfde",
+    "url": "/clip-art/static/css/main.ea938798.chunk.css"
+  },
+  {
+    "revision": "c26c1004b782293bef84",
+    "url": "/clip-art/static/js/2.a5cb57be.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/clip-art/static/js/2.a5cb57be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee49bd88c40a3b27bfde",
+    "url": "/clip-art/static/js/main.b37885ef.chunk.js"
+  },
+  {
+    "revision": "fb6d6b2c842a80533e9c",
+    "url": "/clip-art/static/js/runtime-main.55d51b48.js"
+  }
+]);
